f0048f961136c6bd26d406dd335e0fa6fc9cb3fc
jamtat <ubershinysheep@gmail.com>
2021-01-22 00:14:05 +0000

    Autoupdate -> Fri Jan 22 00:14:05 GMT 2021

--- scripts/1_fetch_data.sh
+++ scripts/1_fetch_data.sh
@@ -2,9 +2,8 @@
 
 set -euxo pipefail
 
+export SSHPASS="$(cat ssh_pass)"
 pushd advancements_json > /dev/null
-
-    export SSHPASS="$(cat ssh_pass)"
     sshpass -e sftp -oBatchMode=no -oStrictHostKeyChecking=no -P 7767 -b - jctops.f2263ca4@eu-ger-06-9900k.node.dedicatedmc.io << EOF
 get world_jan21/advancements/*.json
 get whitelist.json
